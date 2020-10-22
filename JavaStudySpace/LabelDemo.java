6ae57a3452ecad38c2d4717bdd58b9e442df0990
Javachenxu <chenxu135790@outlook.com>
2020-10-22 15:02:34 +0800

    总结

---
+++ JavaStudySpace/LabelDemo.java
@@ -0,0 +1,15 @@
+package JavaStudySpace;
+//控制外层循环
+public class LabelDemo {
+    public static void main(String[] args) {
+        outer: for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <=4 ; j++) {
+                if (i == 3) {
+                    //结束外循环
+                    break outer;
+                }
+                System.out.println("i="+ i+",j=" + j);
+            }
+        }
+    }
+}
