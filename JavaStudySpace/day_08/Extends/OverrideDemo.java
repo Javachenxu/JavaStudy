2d5af3cc9e6807d314778d97c6a81ae37c03f5dc
Javachenxu <chenxu135790@outlook.com>
2020-10-25 13:11:19 +0800

    重写

---
+++ JavaStudySpace/day_08/Extends/OverrideDemo.java
@@ -0,0 +1,21 @@
+package JavaStudySpace.day_08.Extends;
+//方法覆盖演示
+//普通的鸟类
+class bird{
+    public void fly() {
+        System.out.println("能飞");
+    }
+}
+//企鹅类
+class Penguin extends bird{
+    public void fly() {
+        System.out.println("不能飞");
+    }
+}
+public class OverrideDemo {
+    public static void main(String[] args) {
+        Penguin p = new Penguin();
+        p.fly();
+
+    }
+}
