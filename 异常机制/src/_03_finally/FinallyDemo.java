21633ca6d143d488aea40c74be517099c4079eb2
Javachenxu <chenxu135790@outlook.com>
2020-10-31 18:34:22 +0800

    finally关键字

--- 异常机制/src/_03_finally/FinallyDemo.java
+++ 异常机制/src/_03_finally/FinallyDemo.java
@@ -2,8 +2,27 @@ package _03_finally;
 
 public class FinallyDemo {
 
-	public static void main(String[] args) {
-		
+		public static void main(String[] args) {
+	 
+			System.out.println("main 代码块中的执行结果为：" + myMethod());
+		}
+	 
+		public static int myMethod() {
+	 
+			int i = 6;
+			try {
+				System.out.println("try 代码块被执行！");
+	 
+				 i = i/0;
+	 
+				return 1;
+			} catch (Exception e) {
+				System.out.println("catch 代码块被执行！");
+				return 2;
+			} finally {
+				System.out.println("finally 代码块被执行！");
+			}
+	 
+		}
+	 
 	}
-
-}
