fb0e2a1615794b84baa0813b24633f598bc82615
Javachenxu <chenxu135790@outlook.com>
2020-11-04 10:45:21 +0800

    修改

--- testspace/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package testspace;
-import java.util.Scanner;
-public class HelloWorld {
-    int num = 0;//成员变量
-    public static void main(String[] args) {
-        int sum = 0;//局部变量
-        System.out.println("Hello world!");
-    }   
-}
