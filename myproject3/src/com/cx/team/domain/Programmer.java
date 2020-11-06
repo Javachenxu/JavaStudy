8db3f2e84aa0ea4ad93a7601067a504f89c46d84
Javachenxu <chenxu135790@outlook.com>
2020-11-06 23:35:38 +0800

    程序员

---
+++ myproject3/src/com/cx/team/domain/Programmer.java
@@ -0,0 +1,39 @@
+package com.cx.team.domain;
+
+import com.cx.team.service.Status;
+
+public class Programmer extends Employee {
+	private int memberId;
+	private Status status = Status.FREE;
+	private Equipment equipment;
+	public Programmer() {
+		super();
+	}
+	public Programmer(int id, String name, int age, double salary, Equipment equipment) {
+		super(id, name, age, salary);
+		this.equipment = equipment;
+	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public Equipment getEquipment() {
+		return equipment;
+	}
+	public void setEquipment(Equipment equipment) {
+		this.equipment = equipment;
+	}
+	@Override
+	public String toString() {
+		return getDetails() + "\t程序员" + status + "\t\t\t" + equipment.getDescription();
+	}
+	
+}
