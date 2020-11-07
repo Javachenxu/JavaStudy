67ce698e17b704c20a2711540e102edb66210448
Javachenxu <chenxu135790@outlook.com>
2020-11-07 20:01:34 +0800

    测试类

--- myproject3/src/com/cx/team/junit/NameListServiceTest.java
+++ myproject3/src/com/cx/team/junit/NameListServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import com.cx.team.domain.Employee;
 import com.cx.team.service.NameListService;
+import com.cx.team.service.TeamException;
 
 //对NameListService的测试
 public class NameListServiceTest {
@@ -13,6 +14,18 @@ public class NameListServiceTest {
 		Employee[] employees = service.getAllEmployees();
 		for (int i = 0; i < employees.length; i++) {
 			System.out.println(employees[i]);
+		}
+	}
+	@Test
+	public void testGetEmployee() {
+		NameListService service = new NameListService();
+		int id = 1;
+		id = 10;
+		try {
+			Employee employee = service.getEmployee(id);
+			System.out.println(employee);
+		} catch (TeamException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
