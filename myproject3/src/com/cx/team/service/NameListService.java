e626a57a7615403498a17be9f677a03949837871
Javachenxu <chenxu135790@outlook.com>
2020-11-07 20:05:25 +0800

    员工数组

--- myproject3/src/com/cx/team/service/NameListService.java
+++ myproject3/src/com/cx/team/service/NameListService.java
@@ -11,74 +11,73 @@ import com.cx.team.domain.Programmer;
 
 import static com.cx.team.service.Data.*;
 
+
 public class NameListService {
 	private Employee[] employees;
 
 	public NameListService() {
 		employees = new Employee[EMPLOYEES.length];
+
 		for (int i = 0; i < employees.length; i++) {
+			// 获取通用的属性
 			int type = Integer.parseInt(EMPLOYEES[i][0]);
-			//获取Employee的4个基本信息
 			int id = Integer.parseInt(EMPLOYEES[i][1]);
 			String name = EMPLOYEES[i][2];
 			int age = Integer.parseInt(EMPLOYEES[i][3]);
 			double salary = Double.parseDouble(EMPLOYEES[i][4]);
-			
-			Equipment equipment;
+
+			//
+			Equipment eq;
 			double bonus;
 			int stock;
-			//获取四个信息
+
 			switch (type) {
 			case EMPLOYEE:
 				employees[i] = new Employee(id, name, age, salary);
 				break;
 			case PROGRAMMER:
-				equipment = createEquipment(i);
-				employees[i] = new Programmer(id, name, age, salary, equipment);
+				eq = createEquipment(i);
+				employees[i] = new Programmer(id, name, age, salary, eq);
 				break;
 			case DESIGNER:
-				equipment = createEquipment(i);
-				bonus = Double.parseDouble(EMPLOYEES[i][5]);
-				new Designer(id, name, age, salary, equipment, bonus);
+				eq = createEquipment(i);
+				bonus = Integer.parseInt(EMPLOYEES[i][5]);
+				employees[i] = new Designer(id, name, age, salary, eq, bonus);
 				break;
 			case ARCHITECT:
-				equipment = createEquipment(i);
-				bonus = Double.parseDouble(EMPLOYEES[i][5]);
+				eq = createEquipment(i);
+				bonus = Integer.parseInt(EMPLOYEES[i][5]);
 				stock = Integer.parseInt(EMPLOYEES[i][6]);
-				new Architect(id, name, age, salary, equipment, bonus, stock);
+				employees[i] = new Architect(id, name, age, salary, eq, bonus,
+						stock);
 				break;
 			}
 		}
 	}
-	//获取指定index位置上员工的设备
+
 	private Equipment createEquipment(int index) {
-		int key = Integer.parseInt(EQIPMENTS[index][0]);
-		String modelOrName = EQIPMENTS[index][1];
-		
-		switch (key) {
-		case PC://21
-			String display = EQIPMENTS[index][2];
-			return new PC(modelOrName, display);
-		case NOTEBOOK://22
-			double price = Double.parseDouble(EQIPMENTS[index][2]);
-			return new NoteBook(modelOrName, price);
-		case PRINTER://23
-			String type = EQIPMENTS[index][2];
-			return new Printer(type ,modelOrName);
+		int type = Integer.parseInt(EQIPMENTS[index][0]);
+		switch (type) {
+		case PC:
+			return new PC(EQIPMENTS[index][1], EQIPMENTS[index][2]);
+		case NOTEBOOK:
+			int price = Integer.parseInt(EQIPMENTS[index][2]);
+			return new NoteBook(EQIPMENTS[index][1], price);
+		case PRINTER:
+			return new Printer(EQIPMENTS[index][1], EQIPMENTS[index][2]);
 		}
 		return null;
 	}
+
 	public Employee[] getAllEmployees() {
 		return employees;
 	}
-	
+
 	public Employee getEmployee(int id) throws TeamException {
-		for (int i = 0; i < employees.length; i++) {
-			if (employees[i].getId() == id) {
-				return employees[i];
-			}
+		for (Employee e : employees) {
+			if (e.getId() == id)
+				return e;
 		}
-		throw new TeamException("找不到指定的员工");
-		
+		throw new TeamException("该员工不存在");
 	}
 }
