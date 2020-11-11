8421ca6f579ecac8cb1b0e722c91c4b11c161ab6
Javachenxu <chenxu135790@outlook.com>
2020-11-11 23:20:28 +0800

    跳台阶问题

---
+++ test/src/test/TestStep.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestStep {
+	@Test
+	public void test(){
+		System.out.println(fn(3));
+	}
+	//实现f(n):求n步台阶，一共有几种走法
+	public int fn(int n) {
+		if (n < 1) {
+			throw new IllegalArgumentException(n +"不能小于1");
+		}
+		if (n == 1 || n == 2) {
+			return n;
+		}
+		return fn(n - 1) + fn(n - 2);
+	}
+}
