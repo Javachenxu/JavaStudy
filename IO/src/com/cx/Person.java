4086ac21f94258837427d8b4cda8a46493fa9a51
Javachenxu <chenxu135790@outlook.com>
2020-11-15 10:31:49 +0800

    辅助

---
+++ IO/src/com/cx/Person.java
@@ -0,0 +1,37 @@
+package com.cx;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	//标识ID
+	public static final long serialVersionUID = 15555544L;
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public Person() {
+		super();
+	}
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
